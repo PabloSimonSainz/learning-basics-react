0729a6952dbfe2a540ff7aba86d3b2a11a2808c7
Pablo Simón <pablosimonsr@gmail.com>
2022-01-16 21:16:05 +0100

    learning-basics-react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello
+      </header>
+      <body>
+        <h1>
+          Hola Buenas Tardes
+        </h1>
+      </body>
+    </div>
+  );
+}
+
+export default App;
